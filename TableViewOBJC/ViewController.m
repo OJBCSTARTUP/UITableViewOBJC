9fccec4b8a28a923a78a117f3473b5bc659086cb
Nem Sothea <NemSothea@NemSothea.local>
2017-03-10 15:24:10 +0700

    Initial Commit

---
+++ TableViewOBJC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TableViewOBJC
+//
+//  Created by Nem Sothea on 3/10/17.
+//  Copyright © 2017 Nem Sothea. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
